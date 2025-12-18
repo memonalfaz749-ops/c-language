09cb35071fa5bf4268c07d759eaec3ff9e6b89b1
memon alfaz <memonalfaz749@gmail.com>
2025-12-18 11:08:31 +0530

    Added new files

---
+++ home-practice/swapping.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main (){
+	
+	int a,b;
+	
+	a=5;
+	b=10;
+	
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	
+	printf("value of a is : %d",a);
+	printf("value of b is : %d",b);
+	
+	return 0;
+}
