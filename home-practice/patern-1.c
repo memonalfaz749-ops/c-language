70f1ddb6df36b99f543cba1237910cd8d3636abf
memon alfaz <memonalfaz749@gmail.com>
2026-01-05 17:26:42 +0530

    Added new files

---
+++ home-practice/patern-1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+int main (){
+	
+	int i,j;
+	
+	for(i=5;i>=1;i--){
+		for(j=1;j<=i;j++){
+			if(i%2==0){
+				printf("@");
+			}
+			else{
+				printf("#");
+			}
+		}
+		printf("\n");
+	}
+	
+	
+	
+	
+	return 0;
+}
