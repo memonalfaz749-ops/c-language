37bbd4da7acda4a9169527c1f38c93e9609b8db1
memon alfaz <memonalfaz749@gmail.com>
2026-02-08 23:11:33 +0530

    Added new files

---
+++ home-practice/c-10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (){
+	
+	int i;
+	
+   printf("enter your age :");
+   scanf("%d",&i);
+   
+   
+   printf("\nstudent info");
+   printf("\nage :%d",i);	
+
+	
+	return 0;
+}
