9c873c81f28579d47bf2e5defd84bf20f3e19293
memon alfaz <memonalfaz749@gmail.com>
2026-03-07 18:40:36 +0530

    Added new files

--- calculator/Q-1.c
+++ calculator/Q-1.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
 int n1,n2;
+
+int addition();
+int subtraction();
+
 int addition(){
 	
 	printf("Enter the first number :");
@@ -9,6 +13,55 @@ int addition(){
 	scanf("%d",&n2);
 	
 	printf("Addition of %d and %d is %d",n1,n2,n1+n2);
+}
+int subtractin(){
+	
+	printf("Enter the first number :");
+	scanf("%d",&n1);
+	
+	printf("Enter the second number :");
+	scanf("%d",&n2);
+	
+	printf("Subtraction of %d and %d is %d",n1,n2,n1-n2);
+
+}
+
+int multiplication(){
+	
+	printf("Enter the first number :");
+	scanf("%d",&n1);
+	
+	printf("Enter the second number :");
+	scanf("%d",&n2);
+	
+	printf("Multiplication of %d and %d is %d",n1,n2,n1*n2);
+		
+}
+
+int division(){
+	
+	printf("Enter the first number :");
+	scanf("%d",&n1);
+	
+	printf("Enter the second number :");
+	scanf("%d",&n2);
+	
+	printf("Division of %d and %d is %d",n1,n2,n1 / n2);
+	
+	
+}
+
+int modulus(){
+	
+	printf("Enter the first number :");
+	scanf("%d",&n1);
+	
+	printf("Enter the second number :");
+	scanf("%d",&n2);
+	
+	printf("Modulus of %d and %d is %d",n1,n2,n1 % n2);
+	
+	
 }
 int main (){
 	
@@ -33,7 +86,16 @@ int main (){
 		
 		case 1 : addition();break;
 		
+		case 2 : subtractin();break;
+		
+		case 3 : multiplication();break;
+		
+		case 4 : division();break;
+		
+		case 5 : modulus();break;
+		
 		case 0 : printf("Exiting");break;
+		
 		default : printf("Enter valid choice.!!");
 	}
 	
