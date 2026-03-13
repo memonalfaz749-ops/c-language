3496f10775a74a154499a0ef5991a93e0aaa8784
memon alfaz <memonalfaz749@gmail.com>
2026-03-13 22:27:01 +0530

    Added new files

---
+++ home-practice/square-2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int square(int i);
+
+int square(int i){
+	return i*i;
+}
+int main (){
+	
+	
+	
+	printf("square of 4 is %d",square(4));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
